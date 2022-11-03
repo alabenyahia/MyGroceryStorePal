f1d8b60532a95f3c6e62c230bd4e0ac6cf176484
Ala Ben Yahia <alabny18@gmail.com>
2022-11-03 22:18:14 +0100

    Add sign in with google and facebook ui

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Button, FormControl, Heading, Input, Text, WarningOutlineIcon} from "native-base";
-import {Pressable} from "react-native";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {Button, Divider, FormControl, Heading, Input, Text, WarningOutlineIcon} from "native-base";
+import {Image, Pressable, ScrollView, View} from "react-native";
+import {signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import {auth} from "../../config/firebase";
 import {isEmailValid} from "../../utils/functions";
+import facebook from "../../assets/facebook-icon.png";
+import google from "../../assets/google-icon.png";
+
 
 const Login = () => {
     const [email, setEmail] = useState("")
@@ -14,6 +17,8 @@ const Login = () => {
     const [passwordError, setPasswordError] = useState("")
 
     const [loginError, setLoginError] = useState("")
+
+    auth.useDeviceLanguage();
 
     async function login() {
         setEmailError("")
@@ -42,34 +47,57 @@ const Login = () => {
 
     return (
         <SafeAreaView style={{padding: 16}}>
-            <Heading style={{color: "#1F2937"}}>Welcome back,</Heading>
-            <Text style={{color: "#1F2937"}}>Sign in to continue</Text>
+            <ScrollView>
+                <Heading style={{color: "#1F2937"}}>Welcome back,</Heading>
+                <Text style={{color: "#1F2937"}}>Sign in to continue</Text>
 
-            {loginError && <Text style={{color: "#cc0000", textAlign: "center"}} mt={4}>{loginError}</Text>}
+                {loginError && <Text style={{color: "#cc0000", textAlign: "center"}} mt={4}>{loginError}</Text>}
 
-            <FormControl isInvalid={emailError.length > 0} mt={8} mb={3}>
-                <FormControl.Label>Email</FormControl.Label>
-                <Input placeholder="Enter your email" value={email}
-                       onChangeText={(value) => {setEmail(value)}}/>
-                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                    {emailError}
-                </FormControl.ErrorMessage>
-            </FormControl>
+                <FormControl isInvalid={emailError.length > 0} mt={8} mb={3}>
+                    <FormControl.Label>Email</FormControl.Label>
+                    <Input placeholder="Enter your email" value={email}
+                           onChangeText={(value) => {
+                               setEmail(value)
+                           }}/>
+                    <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs"/>}>
+                        {emailError}
+                    </FormControl.ErrorMessage>
+                </FormControl>
 
-            <FormControl isInvalid={passwordError.length > 0} mb={3}>
-                <FormControl.Label>Password</FormControl.Label>
-                <Input placeholder="Enter your password" value={password} type={"password"}
-                       onChangeText={(value) => {setPassword(value)}}/>
-                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                    {passwordError}
-                </FormControl.ErrorMessage>
-            </FormControl>
+                <FormControl isInvalid={passwordError.length > 0} mb={3}>
+                    <FormControl.Label>Password</FormControl.Label>
+                    <Input placeholder="Enter your password" value={password} type={"password"}
+                           onChangeText={(value) => {
+                               setPassword(value)
+                           }}/>
+                    <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs"/>}>
+                        {passwordError}
+                    </FormControl.ErrorMessage>
+                </FormControl>
 
-            <Pressable>
-                <Text style={{textAlign: "right", color: "#F16B44", fontWeight: "bold"}}>Forgot Password?</Text>
-            </Pressable>
+                <Pressable>
+                    <Text style={{textAlign: "right", color: "#F16B44", fontWeight: "bold"}}>Forgot Password?</Text>
+                </Pressable>
 
-            <Button mt={6} onPress={() => login()}>LOGIN</Button>
+                <Button mt={6} onPress={() => login()}>LOGIN</Button>
+
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 56, marginBottom: 40}}>
+                    <Divider style={{width: "30%"}}/>
+                    <Text style={{marginHorizontal: 12, color: "#9CA3AF"}}>or</Text>
+                    <Divider style={{width: "30%"}}/>
+                </View>
+
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+                    <Pressable>
+                        <Image source={facebook}
+                               style={{width: 32, height: 32, alignSelf: 'center', marginRight: 16}}/>
+                    </Pressable>
+                    <Pressable>
+                        <Image source={google}
+                               style={{width: 32, height: 32, alignSelf: 'center'}}/>
+                    </Pressable>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
--- screens/auth/SignUp.js
+++ screens/auth/SignUp.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
-import {Button, FormControl, Heading, Input, Text, WarningOutlineIcon} from "native-base";
+import {Button, Divider, FormControl, Heading, Input, Text, WarningOutlineIcon} from "native-base";
 import {SafeAreaView} from "react-native-safe-area-context";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
 import {auth} from "../../config/firebase";
 import {isEmailValid} from "../../utils/functions";
+import {Image, Pressable, View} from "react-native";
+import facebook from "../../assets/facebook-icon.png";
+import google from "../../assets/google-icon.png";
 
 
 const SignUp = ({navigation}) => {
@@ -47,38 +50,63 @@ const SignUp = ({navigation}) => {
 
     return (
         <SafeAreaView style={{padding: 16}}>
-            <Heading style={{color: "#1F2937"}}>Welcome</Heading>
-            <Text style={{color: "#1F2937"}}>Sign up to continue</Text>
+            <ScrollView>
+                <Heading style={{color: "#1F2937"}}>Welcome</Heading>
+                <Text style={{color: "#1F2937"}}>Sign up to continue</Text>
 
 
-            <FormControl isInvalid={emailError.length > 0} mt={8} mb={3}>
-                <FormControl.Label>Email</FormControl.Label>
-                <Input placeholder="Enter your email" value={email}
-                       onChangeText={(value) => {setEmail(value)}}/>
-                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                    {emailError}
-                </FormControl.ErrorMessage>
-            </FormControl>
+                <FormControl isInvalid={emailError.length > 0} mt={8} mb={3}>
+                    <FormControl.Label>Email</FormControl.Label>
+                    <Input placeholder="Enter your email" value={email}
+                           onChangeText={(value) => {
+                               setEmail(value)
+                           }}/>
+                    <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs"/>}>
+                        {emailError}
+                    </FormControl.ErrorMessage>
+                </FormControl>
 
-            <FormControl isInvalid={passwordError.length > 0} mb={3}>
-                <FormControl.Label>Password</FormControl.Label>
-                <Input placeholder="Enter your password" value={password} type={"password"}
-                       onChangeText={(value) => {setPassword(value)}}/>
-                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                    {passwordError}
-                </FormControl.ErrorMessage>
-            </FormControl>
+                <FormControl isInvalid={passwordError.length > 0} mb={3}>
+                    <FormControl.Label>Password</FormControl.Label>
+                    <Input placeholder="Enter your password" value={password} type={"password"}
+                           onChangeText={(value) => {
+                               setPassword(value)
+                           }}/>
+                    <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs"/>}>
+                        {passwordError}
+                    </FormControl.ErrorMessage>
+                </FormControl>
 
-            <FormControl isInvalid={confirmPasswordError.length > 0} mb={3}>
-                <FormControl.Label>Confirm Password</FormControl.Label>
-                <Input placeholder="Enter your password again" value={confirmPassword} type={"password"}
-                       onChangeText={(value) => {setConfirmPassword(value)}}/>
-                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                    {confirmPasswordError}
-                </FormControl.ErrorMessage>
-            </FormControl>
+                <FormControl isInvalid={confirmPasswordError.length > 0} mb={3}>
+                    <FormControl.Label>Confirm Password</FormControl.Label>
+                    <Input placeholder="Enter your password again" value={confirmPassword} type={"password"}
+                           onChangeText={(value) => {
+                               setConfirmPassword(value)
+                           }}/>
+                    <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs"/>}>
+                        {confirmPasswordError}
+                    </FormControl.ErrorMessage>
+                </FormControl>
 
-            <Button mt={6} onPress={() => signUp()}>SIGN UP</Button>
+                <Button mt={6} onPress={() => signUp()}>SIGN UP</Button>
+
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 56}}>
+                    <Divider style={{width: "30%"}}/>
+                    <Text style={{marginHorizontal: 12, color: "#9CA3AF"}}>or</Text>
+                    <Divider style={{width: "30%"}}/>
+                </View>
+
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+                    <Pressable>
+                        <Image source={facebook}
+                               style={{width: 32, height: 32, alignSelf: 'center', marginRight: 16}}/>
+                    </Pressable>
+                    <Pressable>
+                        <Image source={google}
+                               style={{width: 32, height: 32, alignSelf: 'center'}}/>
+                    </Pressable>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
