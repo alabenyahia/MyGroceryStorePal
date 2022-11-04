c4edec4d088a4caee751432e2bb823e554bc89c8
Ala Ben Yahia <alabny18@gmail.com>
2022-11-04 23:56:41 +0100

    Finish Inventory screen ui

--- components/customDrawerContent.js
+++ components/customDrawerContent.js
@@ -13,8 +13,7 @@ const CustomDrawerContent = (props) => {
                 <Avatar bg="cyan.500" size="lg" source={{
                     uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                 }} mb={4}/>
-                
-                <View>
+                <View >
                     <Select placeholder="Choose a store" mb={8}>
                         <Select.Item label="UX Research" value="ux" />
                         <Select.Item label="Web Development" value="web" />
--- screens/mainscreens/Inventory.js
+++ screens/mainscreens/Inventory.js
@@ -1,11 +1,186 @@
 import React from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Text} from "react-native";
+import {Image, Pressable, ScrollView, View} from "react-native";
+import {Button, Divider, Fab, Icon, Text} from "native-base";
+import InventoryIcon from "../../assets/icons/inventory.png";
+import SellingsIcon from "../../assets/icons/sellings.png";
+import KitkatImg from "../../assets/kitkat.png";
+import {AntDesign} from "@expo/vector-icons";
 
 const Inventory = () => {
     return (
         <SafeAreaView>
-            <Text>kljslks</Text>
+            <ScrollView>
+                <View style={{marginBottom: 32, padding: 16, paddingBottom: 0}}>
+                    <Text bold style={{textAlign: "center"}}>Today</Text>
+                    <Button mt={2} >Select date</Button>
+                </View>
+
+                <Divider/>
+
+                <View style={{flexDirection: "row", justifyContent: "center", marginTop: 32, padding: 16, paddingTop: 0, paddingBottom: 0}}>
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+
+                        marginHorizontal: 12,
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+                </View>
+
+                <Divider mb={8} mt={8}/>
+
+
+                <View style={{flexDirection: "row", justifyContent: "center", padding: 16, paddingTop: 0}}>
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+
+                        marginHorizontal: 12,
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+
+                    <Pressable style={{
+                        width: "30%",
+                        backgroundColor: "#ececec",
+                        borderRadius: 8,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+                        shadowOpacity: 0.22,
+                        shadowRadius: 2.22,
+
+                        elevation: 3,
+                    }}>
+                        <Image source={KitkatImg}
+                               style={{width: "100%", height: 80, alignSelf: 'center'}}/>
+                        <View style={{padding: 6}}>
+                            <Text>KitKat</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>X16</Text>
+                            <Divider mt={1} mb={1}/>
+                            <Text>1.5TND</Text>
+                        </View>
+                    </Pressable>
+                </View>
+                <Fab renderInPortal={false} shadow={3} size="md" icon={<Icon color="white" as={AntDesign} name="plus" size="sm" />} />
+
+            </ScrollView>
         </SafeAreaView>
     );
 };
