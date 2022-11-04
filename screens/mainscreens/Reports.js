d57c490856f79c14125daf9f2bb4e41d9a38c491
Ala Ben Yahia <alabny18@gmail.com>
2022-11-05 00:22:12 +0100

    Finish reports screen ui

--- screens/mainscreens/Reports.js
+++ screens/mainscreens/Reports.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text} from "react-native";
+import {ScrollView, Text} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {Box, Button, Divider, FlatList} from "native-base"
 
 const Reports = () => {
     return (
-        <SafeAreaView>
-            <Text>kljslks</Text>
+        <SafeAreaView style={{padding: 16}}>
+            <ScrollView>
+                <Button mb={4}>Most sold products</Button>
+                <Button mb={6}>Least sold products</Button>
+                <Divider mb={6}/>
+                <Button mb={4}>Most products that made profit</Button>
+                <Button mb={6}>Least products that made profit</Button>
+                <Divider mb={6}/>
+                <Button mb={4}>Profits</Button>
+                <Button>Expenses</Button>
+            </ScrollView>
         </SafeAreaView>
     );
 };
